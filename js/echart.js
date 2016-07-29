b01c15bd7189e7c0c186bb3251b6ff8b8bbadd35
unknown <>
2016-07-29 15:03:43 +0800

    初始版本

--- js/echart.js
+++ js/echart.js
@@ -1,131 +1,79 @@
-// 基于准备好的dom，初始化echarts图表
-var myChart = echarts.init(document.getElementById('main'));
-
-var option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: function(params) {
-            var date = new Date(params.value[0]);
-            data = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
-            return data + '<br/>' + params.value[1];
-        }
-    },
-    //时间轴
-    dataZoom: {
-        show: true,
-        start: 90,
-        end: 100
-    },
-    //折线显示隐藏控制
-    legend: {
-        orient: 'vertical',
-        x: 'left',
-        y: 'center',
-        data: [{ name: 'series1' }, { name: 'series2' }]
-    },
-    //图表位置调整
-    grid: {
-        x: 120,
-        y2: 80
-    },
-    xAxis: [{
-        type: 'time',
-        //横坐标显示的值得个数
-        splitNumber: 10
-    }],
-    yAxis: [{
-        type: 'value',
-        //纵坐标显示的值得个数
-        splitNumber: 10
-    }],
-    //数据
-    series: [{
-        name: 'series1',
-        type: 'line',
-        showAllSymbol: true,
-        symbolSize: function(value) {
-            return 2;
-        },
-        markpoint: {
-            symbol: 'emptyCircle',
-            symbolSize: function(v) {
-                return 10 + v / 10
-            },
-            effect: {
-                show: true,
-                shadowBlur: 0
-            },
-            itemStyle: {
-                normal: {
-                    label: { show: false }
-                },
-                emphasis: {
-                    label: { position: 'top' }
-                }
-            },
-            data: [
-                { name: 'test', value: 100, xAxis: new Date(2014, 9, 14, 0, 0), yAxis: 2100 }
-            ]
-        },
-        data: (
-            function() {
-                var d = [];
-                var len = 0;
-                var now = new Date();
-                var value;
-                while (len++ < 200) {
-                    d.push([
-                        new Date(2014, 9, 1, 0, len * 100),
-                        (Math.random() * 200 + 2000).toFixed(2) - 0
-                    ]);
-                }
-                return d;
+function echart(msg) {
+    // 基于准备好的dom，初始化echarts图表
+    var myChart = echarts.init(document.getElementById('main'));
+    var option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: function(params) {
+                var date = params.value[0];
+                data = params.seriesName + '<br/>' + date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
+                return data + '<br/>' + params.value[1] + '元';
             }
-        )()
-    }, {
-        name: 'series2',
-        type: 'line',
-        showAllSymbol: true,
-        symbolSize: function(value) {
-            return 2;
         },
-        data: (
-            function() {
-                var d = [];
-                var len = 0;
-                var now = new Date();
-                var value;
-                while (len++ < 200) {
-                    d.push([
-                        new Date(2014, 9, 1, 0, len * 100),
-                        (Math.random() * 200 + 2000).toFixed(2) - 0
-                    ]);
-                }
-                return d;
-            }
-        )()
-    }],
-    //工具箱
-    // toolbox: {
-    //     show : true,
-    //     feature : {
-    //         mark : {show: true},
-    //         dataView : {show: true, readOnly: false},
-    //         restore : {show: true},
-    //         saveAsImage : {show: true}
-    //     }
-    // },
+        //时间轴
+        dataZoom: {
+            show: true,
+            start: 0,
+            end: 100
+        },
+        //折线显示隐藏控制
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            y: 'center',
+            data: (function() {
+                var legend = [];
+                msg.data.forEach(function(p) {
+                    legend.push({ name: p.platform });
+                })
+                return legend;
+            })()
+        },
+        //图表位置调整
+        grid: {
+            x: 150,
+            y2: 80,
+        },
+        xAxis: [{
+            type: 'time',
+            //横坐标显示的值得个数
+            splitNumber: 10
+        }],
+        yAxis: [{
+            type: 'value',
+            //纵坐标显示的值得个数
+            splitNumber: 6,
+            scale: true
+        }],
+        //数据
+        series: (function() {
+            var rt = [];
+            msg.data.forEach(function(data) {
+                var list = {
+                    name: data.platform,
+                    type: 'line',
+                    showAllSymbol: true,
+                    symbolSize: function(value) {
+                        return 2;
+                    },
+                    data: (function() {
+                        var d = [];
+                        data.value.forEach(function(line) {
+                            d.push([
+                                new Date(line.year, line.month, line.day, line.hour, line.minute),
+                                line.price
+                            ]);
+                        });
+                        return d;
+                    })()
+                };
+                rt.push(list);
+            });
+            return rt;
+        })(),
+        backgroundColor: '#f2f3f5',
+    };
 
-    //背景颜色，默认透明
-    // backgroundColor: '#808080',
-    backgroundColor: '#f2f3f5',
-    //表标题
-    // title : {
-    //        text : '时间坐标折线图',
-    //        subtext : 'dataZoom支持'
-    //    },
-    //鼠标hover显示效果(详细数据显示)
-};
-
-// 为echarts对象加载数据 
-myChart.setOption(option);
+    // 为echarts对象加载数据 
+    myChart.setOption(option);
+}
